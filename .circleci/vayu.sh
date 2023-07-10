acc619a435d5e886846f5fd138c8b60fa7e84a97
missgoin <mister.goindo@gmail.com>
2023-07-10 23:10:58 +0700

    hagsfsgsh

--- .circleci/vayu.sh
+++ .circleci/vayu.sh
@@ -48,7 +48,7 @@ FINAL_ZIP_ALIAS=Karenulvay-${DATE}.zip
 ##----------------------------------------------------------##
 # Specify compiler.
 
-COMPILER=azure
+COMPILER=lilium
 
 ##----------------------------------------------------------##
 # Specify Linker
@@ -231,13 +231,13 @@ START=$(date +"%s")
 	       CROSS_COMPILE=aarch64-linux-gnu- \
 	       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
 	       LD=${LINKER} \
-	       #LLVM=1 \
-	       #LLVM_IAS=1 \
-	       AR=llvm-ar \
-	       NM=llvm-nm \
-	       OBJCOPY=llvm-objcopy \
-	       OBJDUMP=llvm-objdump \
-	       STRIP=llvm-strip \
+	       LLVM=1 \
+	       LLVM_IAS=1 \
+	       #AR=llvm-ar \
+	       #NM=llvm-nm \
+	       #OBJCOPY=llvm-objcopy \
+	       #OBJDUMP=llvm-objdump \
+	       #STRIP=llvm-strip \
 	       #READELF=llvm-readelf \
 	       #OBJSIZE=llvm-size \
 	       V=$VERBOSE 2>&1 | tee error.log
